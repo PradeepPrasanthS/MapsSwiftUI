2ee89368c1600af6c3804bc991ad369a5cedfd70
Pradeep Prasanth S <pradeep.prasanth@diatoz.com>
2021-04-20 12:08:26 +0530

    Fixed errors

--- MapsSampleApp/ContentView.swift
+++ MapsSampleApp/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct ContentView: View {
+// Pass your latitude and longitude below
+var coord = CLLocationCoordinate2D(latitude: 42.0460, longitude: -87.8861)
+
+// Initialize the region passing the coordinates
+var region: MKCoordinateRegion{
+  return(
+      MKCoordinateRegion(center: coord, latitudinalMeters: 200, longitudinalMeters: 200)
+     )
+}
+
+// Below model is required to show the list of markers or pointers in tha map
+struct Item: Identifiable{
+  var id = UUID()
+    var latitude: Double
+    var longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
+struct ContentView: View{
+    @State var mapRegion = region
+    // Pass the coordinates of the places for which you want to show the marker
+    let a = [Item(latitude: coord.latitude, longitude: coord.longitude)]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      VStack{
+          Text("Your location").font(.title)
+            // Now you can add the maps passing the region and the list of annotationItems
+            Map(coordinateRegion: $mapRegion, annotationItems: a){ point in
+              MapMarker(coordinate: point.coordinate)
+          }
+        }
     }
 }
 
