735855d854e500aae1e1ca5537b17c4c0ae7d11a
Pradeep Prasanth S <pradeep.prasanth@diatoz.com>
2021-04-20 11:15:38 +0530

    Initial Commit

---
+++ MapsSampleApp/MapsSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsSampleAppApp.swift
+//  MapsSampleApp
+//
+//  Created by Pradeep Prasanth on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MapsSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
